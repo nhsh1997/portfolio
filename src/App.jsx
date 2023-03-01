47bda05c6c167e9615dc725ee8e2274f5dbe3d39
nhsh1997 <nhsh1997@gmail.com>
2023-03-01 14:22:08 +0700

    update scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
-import {useLayoutEffect, useRef, useState} from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import gsap from 'gsap'
-import {OrbitControls, useGLTF} from '@react-three/drei'
-function Chair(props) {
-    const ref = useRef(null)
-    const tl = useRef();
-    const model = useGLTF('chair.glb')
-
-    const toggleTimeline = () => {
-        tl.current.reversed(!tl.current.reversed());
-    };
-
-    useLayoutEffect(() => {
-        const ctx = gsap.context((self) => {
-            tl.current = gsap
-                .timeline()
-                .to(ref.current.rotation,  {duration: 20, y: ref.current.rotation.y + Math.PI * 2})
-                .reverse();
-        }, ref);
-        return () => ctx.revert();
-    }, []);
-
-    return (
-        <primitive onClick={toggleTimeline} rotation-y={ Math.PI } position={ [ -1, -2, -1 ] } ref={ref}  scale={ 1.5 } object={ model.scene } />
-    )
-}
+import {OrbitControls} from '@react-three/drei'
+import Room from "./models/Room";
 
 function App() {
   return (
         <Canvas>
             <OrbitControls makeDefault />
             <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-            <Chair />
+            <Room />
         </Canvas>
   )
 }
